c94691fd133272c566dd71351139e1cfcb4f518b
aziziyah21 <tadzkirohaziziyah@student.telkomuniversity.ac.id>
2025-10-01 22:38:35 +0700

    Update no3.cpp

--- MODUL 1 - Pengenalan Bahasa C++/Unguided/no3.cpp
+++ MODUL 1 - Pengenalan Bahasa C++/Unguided/no3.cpp
@@ -2,17 +2,29 @@
 using namespace std;
 
 int main() {
-    int n;
-    cout << "Masukan input: ";
-    cin >> n;
+    int a;
+    cin >> a;
 
-    for (int i = n; i >= 1; i--) {
-        for (int j = i; j >= 1; j--) cout << j << " ";
-        cout << "* "; //kiri
-
-        for (int j = 1; j <= i; j++) cout << j << " ";
+    for (int i = a; i > 0; i--) 
+    {
+        for (int x = 0; x < a-i; x++)
+        {
+            cout << "  ";
+        }
+        for (int y = i; y > 0; y--)
+        {
+            cout << y << " ";
+        }
+        cout << "* ";
+        for (int z = 1; z <= i; z++)
+        {
+            cout << z << " ";
+        }
         cout << endl;
     }
+    for (int i = 0; i < a; i++)
+    {
+        cout << "  ";
+    }
     cout << "*" << endl;
-    return 0;
 }
