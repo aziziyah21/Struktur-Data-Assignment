28a8eba3c0d915de0f4d0530acaaf7818ca327d2
aziziyah21 <tadzkirohaziziyah@student.telkomuniversity.ac.id>
2025-10-30 04:38:11 +0700

    Add files via upload

---
+++ MODUL 5 - Singly Linked List (Bagian Kedua)/Unguided/main.cpp
@@ -0,0 +1,59 @@
+#include "listSepatu.h"
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    linkedlist List;
+    address nodeA, nodeB, nodeC, nodeD, nodeE = Nil;
+    createList(List);
+
+    dataSepatu dtSepatu;
+
+    nodeA = alokasi("Aerostreet", 158, 250000);
+    nodeB = alokasi("Brodo", 121, 420000);
+    nodeC = alokasi("Rebook", 98, 370000);
+    nodeD = alokasi("Nineten", 69, 415000);
+    nodeE = alokasi("Adidas", 36, 650000);
+
+    insertFirst(List, nodeA);
+    insertLast(List, nodeB);
+    insertAfter(List, nodeC, nodeA);
+    insertAfter(List, nodeD, nodeC);
+    insertLast(List, nodeE);
+
+    cout << "--- ISI LIST SETELAH DILAKUKAN INSERT ---" << endl;
+    printList(List);
+    cout << "Jumlah node : " << nbList(List) << endl;
+    cout << endl;
+
+   updateFirst(List);
+   updateLast(List);
+   updateAfter(List, nodeD);
+
+    cout << "--- ISI LIST SETELAH DILAKUKAN UPDATE ---" << endl;
+    printList(List);
+    cout << "Jumlah node : " << nbList(List) << endl;
+    cout << endl;
+
+    FindNodeByData(List, "Kanky");
+    FindNodeByAddress(List, nodeC);
+    FindNodeByRange(List, 6600, 276000);
+
+    delFirst(List);
+    delLast(List);
+    delAfter(List, nodeD, nodeC);
+
+    cout << "--- ISI LIST SETELAH DILAKUKAN DELETE ---" << endl;
+    printList(List);
+    cout << "jumlah node : " << nbList(List) << endl;
+    cout << endl;
+
+    deleteList(List);
+    cout << "--- ISI LIST SETELAH DILAKUKAN HAPUS LIST ---" << endl;
+    printList(List);
+    cout << "jumlah node : " << nbList(List) << endl;
+    cout << endl;
+
+    return 0;
+}
