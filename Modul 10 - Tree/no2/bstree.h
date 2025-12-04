ef7d3626df85db804f112e572c9fa93c0329e434
aziziyah21 <tadzkirohaziziyah@student.telkomuniversity.ac.id>
2025-12-04 21:38:33 +0700

    Add files via upload

---
+++ Modul 10 - Tree/no2/bstree.h
@@ -0,0 +1,26 @@
+#ifndef BSTREE_H
+#define BSTREE_H
+#define Nil NULL
+
+using namespace std;
+
+typedef int infotype;
+
+struct Node{
+    infotype info;
+    Node *left;
+    Node *right;
+};
+
+typedef Node* address;
+
+address alokasi(infotype x);
+void insertNode(address &root, infotype x);
+address findNode(infotype x, address root);
+void printInorder(address root);
+
+int hitungJumlahNode(address root);
+int hitungTotalInfo(address root);
+int hitungKedalaman(address root, int start);
+
+#endif
